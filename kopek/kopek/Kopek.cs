6403d6a2d0dbc049585be0ee56242b32266acbaf
Verda Aslan <153285355+verdasln@users.noreply.github.com>
2025-04-12 12:57:09 +0300

    Add files via upload

---
+++ kopek/kopek/Kopek.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kopek
+{
+    public class Kopek
+    {
+        public string cinsi;
+        public int yasi;
+        public string renk;
+        public double kilosu;
+
+        public Kopek(int yasi)
+        {
+            Console.WriteLine("yasi");
+            this.yasi = yasi;
+        }
+
+        public Kopek(double kilosu)
+        {
+            Console.WriteLine("kilosu");
+            this.kilosu = kilosu;
+        }
+        public Kopek(double kilosu, int yasi)
+        {
+            Console.WriteLine("Cift Parametreli Yapici");
+            this.yasi = yasi;
+            this.yasi = yasi;
+        }
+        private int insanYasiniHesapla()
+        {
+            return (yasi) * 6;
+        }
+        public void ozellikYazdir()
+        {
+            Console.WriteLine("Köpegin cinsi " + cinsi);
+            Console.WriteLine("Köpegin rengi " + renk);
+            Console.WriteLine("Köpegin yasi " + yasi);
+            Console.WriteLine("Köpegin kilosu " + kilosu);
+            Console.WriteLine("Köpegin insan yasi " + insanYasiniHesapla());
+
+        }
+    }
+}
---
+++ kopek/kopek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kopek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kopek a1 = new Kopek(3);
+            a1.yasi = 3;
+            a1.cinsi = "golden";
+            a1.kilosu = 15;
+            a1.renk = "turuncu";
+            a1.ozellikYazdir();
+        }
+    }
+}
